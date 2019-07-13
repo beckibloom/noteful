f241f81a4dde61cac58ebb3b481784a5c99a86b5
Becki Blumer <rblumer13@gmail.com>
2019-07-13 18:22:42 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main className='App'>
+        <Header />
+        
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className='Header'>
+        <h1>Noteful</h1>
+      </main>
+    );
+  }
+}
+
+export default Header;
