b67a07732971be091d3515573c7cc1bb0060c46d
Becki Blumer <rblumer13@gmail.com>
2019-09-08 13:21:32 -0500

    updated endpoint urls in config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    folders_endpoint: 'http://localhost:9090/folders',
-    notes_endpoint: 'http://localhost:9090/notes',
+    CI: process.env.REACT_APP_CI,
+    folders_endpoint: 'https://radiant-falls-51096.herokuapp.com/api/folders',
+    notes_endpoint: 'https://radiant-falls-51096.herokuapp.com/api/notes',
 }
